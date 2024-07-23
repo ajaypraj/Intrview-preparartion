d76887b13640a7014e7da5bce0ae827ba173c335
Ajay Prajapati <34944605+ajaypraj@users.noreply.github.com>
2024-07-23 21:02:48 +0530

    Create flask_learning.py

---
+++ flask_learning.py
@@ -0,0 +1,21 @@
+#Here are post call done the api
+"""
+253  curl http://127.0.0.1:5000/users
+  254  curl -X POST -H "Content-Type: application/json" -d '{"name":"John Doe","email":"johndoe@example.com"}' http://localhost:5000/users
+  255  curl -X POST -H "Content-Type: application/json" -d '{"name":"Ajay Prajapati","email":"ajay.prajapati2691@gmail.com"}' http://localhost:5000/users
+  256  curl http://127.0.0.1:5000/users
+  257  curl http://127.0.0.1:5000/users/1
+  258  curl -X POST -H "Content-Type: application/json" -d '{"name":"Geeta Prajapati","email":"geeta.prajapati2691@gmail.com"}' http://localhost:5000/users
+  259  curl -X POST -H "Content-Type: application/json" -d '{"name":"john","email":"johny@gmail.com"}' http://localhost:5000/users
+  260  curl http://127.0.0.1:5000/users/
+  261  curl http://127.0.0.1:5000/users
+  262  curl http://127.0.0.1:5000/users/2
+  263  curl http://127.0.0.1:5000/users/3
+  264  curl http://127.0.0.1:5000/users/20
+  265  curl http://127.0.0.1:5000/users
+  266  curl -X PUT -H "Content-Type: application/json" -d '{"name":"Veena","email":"veena@example.com"}' http://localhost:5000/users/4
+  267  curl http://127.0.0.1:5000/users
+  268  curl -X POST -H "Content-Type: application/json" -d '{"name":"johnd","email":"johslcny@gmail.com"}' http://localhost:5000/users
+  269  curl http://127.0.0.1:5000/users
+  270  curl -X DELETE  http://localhost:5000/users/4
+"""
